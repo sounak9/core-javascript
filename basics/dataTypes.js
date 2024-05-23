bc8138a9635981c33eb8848d2b24d7256802af0f
sounak9 <sounakbera9@gmail.com>
2024-05-23 09:52:24 +0530

    dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all the JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "tatai"; // string
+let age = 25; // number
+let isMarried = true; // boolean
+/* premitive dataTypes */
+// number => range of 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not assigned value
+// symbol => unique
+
+console.log(typeof age);
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
