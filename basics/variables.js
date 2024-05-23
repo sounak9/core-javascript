48fdffd0f9f0f06371ed830849c6a7f714846734
sounak9 <sounakbera9@gmail.com>
2024-05-23 09:28:06 +0530

    first-commit

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 14523;
+let accountEmailn = "sounakbera9@gmail.com";
+var accountPassword = "123456";
+
+accountCity = "kolkata";
+
+accountCity = "howrah";
+accountEmailn = "tatai@gmail.com";
+accountPassword = " 2020121";
+
+console.log(accountCity);
+/*
+prefer no to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmailn, accountPassword, accountCity]);
