d7d8f263e098e9bfc3f1cf1fa196c4de443ba41f
sounak9 <sounakbera9@gmail.com>
2024-08-23 10:11:56 +0530

    conversion

---
+++ basics/conversionOperations.js
@@ -0,0 +1,23 @@
+let score = "33";
+
+console.log(typeof score); // string
+
+let valueInNumber = Number(score); // this is the conversion operation
+
+console.log(typeof valueInNumber); // number
+
+// "33" => string
+// 33 => number
+// "33abc" => NaN
+// true => 1 , false => 0;
+
+let booleanValue = 1;
+
+let isBooleanValue = Boolean(booleanValue);
+// 1 => true
+// 0 => false
+// '' = false
+// null = false
+//"tatai" = true
+
+console.log(isBooleanValue);
