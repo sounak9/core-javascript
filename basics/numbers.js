cd175a1965fa5b32f6b1cd66c525661af41a9acc
sounak9 <sounakbera9@gmail.com>
2024-09-01 11:10:05 +0530

    numbers

---
+++ basics/numbers.js
@@ -0,0 +1,14 @@
+const score = 100;
+// console.log(score); // defines number automatically.
+
+const balance = new Number(100);
+console.log(balance);
+// defines number as object
+
+console.log(balance.toString().length);
+
+const otherNumber = 23.58967;
+console.log(otherNumber.toFixed(2)); // 23.59
+
+const numbers = 10000000;
+console.log(numbers.toLocaleString()); // 10,000,000
--- basics/stings.js
+++ basics/stings.js
@@ -3,3 +3,8 @@ const name = "John";
 const age = 30;
 
 console.log(`my name is ${name} and my age is ${age}`);
+
+// declare string as a object
+
+const myName = new String("sounak");
+console.log(myName.length); // output 6
