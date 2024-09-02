1eeb428508acbd610a5079c14a6246740ffc2cd3
sounak9 <sounakbera9@gmail.com>
2024-09-02 13:03:52 +0530

    changes

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -5,5 +5,15 @@ const arr2 = new Array(1, 2, 3, 4); // defining an array as object
 // console.log(arr2);
 
 // Array methods
-myArr.push(6);
-console.log(myArr); // adding an element to the end of the array
+// myArr.push(6);
+// myArr.push(7);
+// console.log(myArr); // adding an element to the end of the array
+// myArr.pop();// deleting the last element of the array
+// myArr.unshift(0); // adding the element in the first index
+// myArr.shift(); // removing the first element in an array
+
+// console.log(myArr.includes(9)); // returns the boolean value as it checks the value present in the array or not
+
+// const newArr = myArr.join(); // converts the array elements into string and returns the comma seperated values
+
+// console.log(newArr);
