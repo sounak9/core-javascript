1c0ade817747e55c57943b2f54b2b757c474a032
sounak9 <sounakbera9@gmail.com>
2024-09-02 12:46:41 +0530

    array

---
+++ 02_basics/arrays.js
@@ -0,0 +1,9 @@
+const myArr = [0, 1, 2, 3, 4, 5]; // declearing an array
+// console.log(myArr[1]); // indexing an array values
+
+const arr2 = new Array(1, 2, 3, 4); // defining an array as object
+// console.log(arr2);
+
+// Array methods
+myArr.push(6);
+console.log(myArr); // adding an element to the end of the array
