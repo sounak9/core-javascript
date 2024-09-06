7e03b64203e5daf66e59386269a4e4c04b9e5428
sounak9 <sounakbera9@gmail.com>
2024-09-06 13:45:25 +0530

    functions

---
+++ 03_functions/functions.js
@@ -0,0 +1,11 @@
+const user = {
+  name: "tatai",
+  price: 199,
+};
+
+function handleObject(anyObject) {
+  console.log(`username is ${anyObject.name} & price is ${anyObject.price}`);
+  return;
+}
+
+handleObject(user);
